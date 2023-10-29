10458858ea3c15dfee5ac4ea75cba7d064b8309a
Michel Mendoza <contacto@arycer.me>
2023-10-29 15:53:39 +0100

    Create echo.js

---
+++ commands/echo.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const data = new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Repite un mensaje.')
+    .addStringOption(option => option.setName('mensaje').setDescription('Mensaje a repetir').setRequired(true));
+
+module.exports = {
+    data: data,
+    async execute (interaction) {
+        if (!interaction.guild) return interaction.deleteReply();
+
+        // Verificar si el usuario tiene el permiso de gestionar mensajes
+        if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
+            return interaction.reply({ content: 'No tienes permiso para usar este comando.', ephemeral: true });
+        }
+
+        const message = interaction.options.getString('mensaje');
+        interaction.channel.send(message);
+    }
+}
