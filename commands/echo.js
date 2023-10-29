5b793ecd28b5ac3ab63d1b720cce5d331c3ff233
Michel Mendoza <contacto@arycer.me>
2023-10-29 15:57:29 +0100

    Update echo.js

--- commands/echo.js
+++ commands/echo.js
@@ -10,15 +10,12 @@ module.exports = {
     async execute (interaction) {
         if (!interaction.guild) return interaction.deleteReply();
 
-        // Verificar si el usuario tiene el permiso de gestionar mensajes
         if (!interaction.member.permissions.has('MANAGE_MESSAGES')) {
             return interaction.followUp({ content: 'No tienes permiso para usar este comando.', ephemeral: true });
         }
 
         const message = interaction.options.getString('mensaje');
         interaction.channel.send(message);
-
-        interaction.followUp({ content: 'Mensaje enviado.', ephemeral: true });
-        interaction.channel.messages.fetch(interaction.id).then(message => message.delete());
+        interaction.deleteReply();
     }
 }
